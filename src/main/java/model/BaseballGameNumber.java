e6c2386c74d1ae2df74ae815cb8741e330d1c7e4
sinsehwan <sk124590@gmail.com>
2026-02-08 00:11:19 +0900

    chore(model): refactor baseballGameNumber compare logic

--- src/main/java/model/BaseballGameNumber.java
+++ src/main/java/model/BaseballGameNumber.java
@@ -10,42 +10,60 @@ public class BaseballGameNumber {
         this.number = arr;
     }
 
-
     public GameAnswerType compare(BaseballGameNumber other) {
-        int strikes = 0;
-        int balls = 0;
-
-        boolean[] isDuplicated = new boolean[10];
-
-        for (int i = 0; i < number.size(); i++) {
-            if (isDuplicated[number.get(i)]) {
-                continue;
-            }
-
-            if (Objects.equals(number.get(i), other.number.get(i))) {
-                strikes += 1;
-            }
-            else if (isContains(other.number.get(i))){
-                balls += 1;
-            }
-
-            isDuplicated[number.get(i)] = true;
-        }
+        int strikes = calStrikes(other);
+        int balls = calBalls(strikes, other);
 
         return new GameAnswerType(strikes, balls);
     }
 
-    private boolean isContains(int num) {
-        for (int n : this.number) {
-            if (isSame(n, num)) {
-                return true;
+    private int calStrikes(BaseballGameNumber other) {
+        int strike = 0;
+        for (int i = 0; i < this.number.size(); i++) {
+            if(isStrike(i, other)) {
+                strike += 1;
             }
         }
-        return false;
+
+        return strike;
     }
 
-    private boolean isSame(int num1, int num2) {
-        return num1 == num2;
+    private int calBalls(int strikes, BaseballGameNumber other) {
+        return this.calMatchCount(other) - strikes;
+    }
+
+    private boolean isStrike(int index, BaseballGameNumber other) {
+        return Objects.equals(this.number.get(index), other.number.get(index));
+    }
+
+    private int calMatchCount(BaseballGameNumber other) {
+        boolean[] originFlags = this.getNumberFlags();
+        boolean[] otherFlags = other.getNumberFlags();
+
+        int matchCount = 0;
+
+        for (int i = 0; i < originFlags.length; i++) {
+            matchCount = getMatchCount(originFlags, i, otherFlags, matchCount);
+        }
+
+        return matchCount;
+    }
+
+    private static int getMatchCount(boolean[] originFlags, int i, boolean[] otherFlags, int matchCount) {
+        if(originFlags[i] && otherFlags[i]) {
+            matchCount += 1;
+        }
+        return matchCount;
+    }
+
+    private boolean[] getNumberFlags() {
+        boolean[] flags = new boolean[10];
+
+        for (int num : this.number) {
+            flags[num] = true;
+        }
+
+        return flags;
     }
 
 }
