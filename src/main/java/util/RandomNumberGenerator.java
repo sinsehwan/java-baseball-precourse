041bcd8f13edb7c9b8112640eb9cc776d0bce117
sinsehwan <sk124590@gmail.com>
2026-02-07 22:41:09 +0900

    chore(util): remove duplicated number in random generator

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -10,13 +10,25 @@ public class RandomNumberGenerator {
     }
 
     public int makeRand3digit() {
+        boolean[] isDuplicated = new boolean[10];
+
         int answer = 0;
 
         for (int i = 0; i < 3; i++) {
             answer *= 10;
-            answer += random.nextInt(9) + 1;
+            answer += getNonDuplicatedNumber(isDuplicated);
         }
 
         return answer;
+    }
+
+    private int getNonDuplicatedNumber(boolean[] isDuplicated) {
+        while(true) {
+            int randNum = random.nextInt(9) + 1;
+            if (!isDuplicated[randNum]) {
+                isDuplicated[randNum] = true;
+                return randNum;
+            }
+        }
     }
 }
