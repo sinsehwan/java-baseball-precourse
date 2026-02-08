ac18f725a07539d7de4b586a2d1bc6b45d3a313e
sinsehwan <sk124590@gmail.com>
2026-02-08 12:47:40 +0900

    fix: test class name

--- src/test/java/model/BaseballGameNumberTest.java
+++ src/test/java/model/BaseballGameNumberTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class BaseBallGameNumberTest {
+public class BaseballGameNumberTest {
 
     @Test
     @DisplayName("3 스트라이크 체크")
